46132f03d3425239ece47c21e500a28f2f47ac76
Giorgio Natili <gnatili@akamai.com>
2017-05-08 11:50:50 -0400

    Fix navigation to the messages view

--- CryptoMessages/authentication/routing/AuthenticationRouting.swift
+++ CryptoMessages/authentication/routing/AuthenticationRouting.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class AuthenticationRouter: AuthenticationRouting {
+    
+    var window: UIWindow?
+    
+    enum SegueIdentifiers: String {
+        
+        case MesseagesList = "MesseagesList"
+    }
+    
+    init() {
+        
+        self.window = UIApplication.shared.delegate!.window!
+    }
+    
+    func presentMessages() {
+        
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = UINavigationController()
+     
+        let view = mainStoryboard.instantiateViewController(withIdentifier: SegueIdentifiers.MesseagesList.rawValue) as! MessagesViewController
+        
+        navigationController.pushViewController(view, animated: false)
+        window?.rootViewController = navigationController
+    }
+}
