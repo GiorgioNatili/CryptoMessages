56a5b3607fa5d3f9d5df06920df6d904254e5ffd
Giorgio Natili <gnatili@akamai.com>
2017-05-07 22:31:32 -0400

    Fix the localization infrastructure

--- CryptoMessages/extensions/Strings+Extension.swift
+++ CryptoMessages/extensions/Strings+Extension.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+extension String {
+    
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
+}
